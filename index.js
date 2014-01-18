465b3b6512fa0e61cb71b279df9d7a698a7423ac
Manuel Cabral <m.cabral@digisfera.pt>
2014-01-18 15:27:52 +0000

    Renamed all `gut` to `tu`

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var path = require('path');
 var coffee = require('coffee-script');
 
 
-module.exports = function(gutfilename) {
-  var gutfile = null;
+module.exports = function(tufilename) {
+  var tufile = null;
 
-  var gutfilefolder = process.cwd();
+  var tufilefolder = process.cwd();
 
 
   do {
     try {
-      moduleToRequire = path.join(gutfilefolder, gutfilename)
-      gutfile = require(moduleToRequire);
+      moduleToRequire = path.join(tufilefolder, tufilename)
+      tufile = require(moduleToRequire);
     }
     catch(e) {
       if(e.code != 'MODULE_NOT_FOUND' || e.message.indexOf(moduleToRequire) === -1) {
@@ -19,21 +19,21 @@ module.exports = function(gutfilename) {
       }
     }
     finally {
-      gutfilefolder = path.dirname(gutfilefolder);
+      tufilefolder = path.dirname(tufilefolder);
     }
   }
-  while(!gutfile && path.dirname(gutfilefolder) != gutfilefolder);
+  while(!tufile && path.dirname(tufilefolder) != tufilefolder);
 
-  if(!gutfile) {
-    throw(new Error("Gutfile not found"));
+  if(!tufile) {
+    throw(new Error("tufile not found"));
   }
 
   var task = process.argv[2];
-  var taskFun = gutfile[task];
+  var taskFun = tufile[task];
   if(!taskFun){
     var err = new Error("No such task: " + task);
     err.code = 'NO_SUCH_TASK';
-    err.tasks = Object.keys(gutfile);
+    err.tasks = Object.keys(tufile);
     throw(err);
   }
   else {
